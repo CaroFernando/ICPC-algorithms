6267ef49c10f2742eddfbe8a347524e4e426ffef
CaroFernando <luisfer.caro@hotmail.com>
2020-10-05 19:22:13 -0500

    template caro

---
+++ templates/caro.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define forn(i, n) for(int i = 0; i < n; i++)
+#define forw(i, n) for(int i = n; i >= 0; i--)
+#define dforn(i, a, b) for(int i = a; i < b; i++)
+#define dforw(i, b, a) for(int i = b; i >= a; i--)
+#define read(v) for(auto &i:v) cin >> i;
+#define all(v) v.begin(), v.end()
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef pair<int, int> pii;
+
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cout.tie(0);cin.tie(0);
+
+	return 0;
+}
