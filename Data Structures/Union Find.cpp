4a715ae84fd4811694a1b39e013a8c669ccdce91
Gamapro <0223826@up.edu.mx>
2020-10-12 08:02:54 -0500

    HOLA, ASIES

--- Data Structures/Union Find.cpp
+++ Data Structures/Union Find.cpp
@@ -1,33 +1,42 @@
-class UnionFind {
-private: 
-    vi p, rank;
+class UnionFind{
+private:
+	vector<int> p, rank, setSizes;
+	int numSets;
 public:
-	UnionFind(){ }
-    UnionFind(int N) { 
-        rank.assign(N, 0); p.assign(N, 0); 
-        for (int i = 0; i < N; i++) p[i] = i; 
-    }
-    int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
-    bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
-    void unionSet(int i, int j) {
-        if (!isSameSet(i, j)) {
-        int x = findSet(i), y = findSet(j);
-        if (rank[x] > rank[y]) p[y] = x;
-        else { p[x] = y;
-        if (rank[x] == rank[y]) rank[y]++; }
-        } 
-    }
-   	vi cant(int N){
-		map <int,int> cont;
-		for(int i=0;i<N;i++){
-			cont[p[i]]++;
+	UnionFind(int N) {
+		numSets = N;
+		rank.assign(N, 0);
+		p.assign(N, 0);
+		for (int i = 0; i < N; i++)
+			p[i] = i;
+		setSizes.assign(N, 1);
+	}
+	int findSet(int i) {
+		return (p[i] == i) ? i : p[i] = findSet(p[i]);
+	}
+	bool isSameSet(int i, int j) {
+		return findSet(i) == findSet(j);
+	}
+	void unionSet(int i, int j) {
+		int x = findSet(i), y = findSet(j);
+		if (!(x==y)) {
+			if (rank[x] > rank[y]) {
+				setSizes[findSet(x)] += setSizes[findSet(y)];
+				p[y] = x;
+			} else {
+				setSizes[findSet(y)] += setSizes[findSet(x)];
+				p[x] = y;
+				if (rank[x] == rank[y])
+					rank[y]++;
+			}
+			numSets--;
 		}
-		vi imp;
-		for(map<int,int> :: iterator it = cont.begin(); it!=cont.end(); ++it){
-			imp.pb(it->se);
-		}
-		return imp;
-	}	
+	}
+	int setSize(int i) {
+		return setSizes[findSet(i)];
+	}
+	int numDisjointSets() {
+		return numSets;
+	}
 };
-
 
